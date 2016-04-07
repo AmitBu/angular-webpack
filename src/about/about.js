b6affc0373654e10ce0b0de7d8cad2987e8daa28
Amit Bourmad <amitb@taykey.com>
2016-04-07 17:06:31 +0300

    Added custom addon changes

--- src/about/about.js
+++ src/about/about.js
@@ -1,18 +1,15 @@
 import angular from 'angular';
-// import appName from '../app/app';
 
-export default
-    angular.module("app.about", [])
-        .component('about', {
-               bindings: {
-                   count: '='
-               },
-               template: require('./about.html'),
-               controller: function() {
-                   // $ngOnInit = function() {
-                   //
-                   // }
-                   debugger;
-                   this.harta = "test";
-               }
-        }).name;
+let options =  {
+    bindings: {
+        count: '='
+    },
+    template: require('./about.html'),
+    controllerAs: "about",
+    controller: function() {
+        this.name = "Amit";
+    }
+};
+
+export default angular.module("app.about", [])
+    .component('about', options).name;
